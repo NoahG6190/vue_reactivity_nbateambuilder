74363af63a3f88f6244f04a54fbc0247f0e4f1ae
NoahG6190 <noahg258@nycstudents.net>
2026-02-11 14:02:05 -0500

    new code 2/11/26

--- nba-team-builder/src/store/TeamStore.js
+++ nba-team-builder/src/store/TeamStore.js
@@ -1,1 +1,15 @@
 import { reactive, computed } from 'vue'
+const state = reactive({
+  team: [],
+})
+
+export function useTeamStore() {
+  const addPlayer = (player) => {
+    if (state.team.length < 5) return
+    if (state.team.some((p) => p.id === player.id)) return
+    state.team.push(player)
+  }
+}
+const removePlayer = (id) => {
+  state.team = state.team.filter((p) => p.id !== id)
+}
