ddfa4a0068d2ae82184c99451c0a72134ad4f54b
NoahG6190 <noahg258@nycstudents.net>
2026-03-03 13:23:26 -0500

    new code

---
+++ nba-team-builder/src/components/nbateam.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class ="team">
+        <h2>My Team</h2>
+        <div v-for="player in team" :key="player.id">{{ player.name }}</div>
+    </div>
+    <div>
+    </div>
+</template>
+
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="css" scoped>
+
+</style>
